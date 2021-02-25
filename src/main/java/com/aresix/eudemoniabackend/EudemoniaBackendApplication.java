1cdfcd392e245dee5188783f8337eab7c2380f68
Aresix <ares_six@hotmail.com>
2021-02-25 12:12:21 +0800

    Initial commit

---
+++ src/main/java/com/aresix/eudemoniabackend/EudemoniaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aresix.eudemoniabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EudemoniaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EudemoniaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aresix/eudemoniabackend/EudemoniaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aresix.eudemoniabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EudemoniaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
